453170bf862eb1386b7c23cb6bfdb0fc338706c3
github username <github email address>
2019-03-07 14:49:04 -0500

    OLD BUT GOOD WAY

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,15 +5,17 @@ before_action :get_order, only: [:show]
   def index
     @orders = Order.all
 
-    @my_orders = @orders.select do |order|
-      order.user == @logged_in_user
-    end
+    # @my_orders = @orders.select do |order|
+    #   order.user == @logged_in_user
+    # end
   end
 
   def show
-    @my_orders = @orders.select do |order|
-      order.user == @logged_in_user
-    end
+    @orders = Order.all
+
+     @my_orders = @orders.select do |order|
+       order.user == @logged_in_user
+     end
   end
 
 
@@ -22,9 +24,15 @@ before_action :get_order, only: [:show]
     @day = Day.find(order_params[:day_id])
     if @order = Order.create(muffin_type_id: order_params[:muffin_type_id], user_id: @logged_in_user.id)
         @order_day = OrderDay.create(order_id: @order.id, day_id: @day.id)
-      flash[:message] = "Great Selection #{@day.name} you will get #{@order.muffin_type.flavor}"
-    redirect_to user_path(@logged_in_user.id)
-    end
+        flash[:message] = "Great Selection #{@day.name} you will get #{@order.muffin_type.flavor}"
+byebug
+      if @order.count = 1
+
+        redirect_to order_path
+      else
+        redirect_to user_path(@logged_in_user.id)
+      end
+     end
   end
 
 private
